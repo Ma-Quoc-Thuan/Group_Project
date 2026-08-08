62a2c9e7302d2a5c5f3678245bc3a29fef45c38e
Ma Quoc Thuan <2512751437@student.hcmus.edu.vn>
2026-08-08 23:28:43 +0700

    Add files via upload

---
+++ build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/CMakeFiles/project_OOP_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_OOP_autogen"
+  "project_OOP_autogen/mocs_compilation.cpp"
+  "project_OOP_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/project_OOP_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
