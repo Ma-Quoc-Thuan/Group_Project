d842cf751fbb3a4b46bdd4848879e64fe1c65828
Ma Quoc Thuan <2512751437@student.hcmus.edu.vn>
2026-08-16 20:47:47 +0700

    Add files via upload

---
+++ build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/CMakeFiles/project_OOP.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "project_OOP_autogen/timestamp" "custom" "project_OOP_autogen/deps"
+  "C:/Users/hqlapsg/Downloads/QuanlySinhVien/build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/project_OOP_autogen/3YJK5W5UP7/qrc_resources.cpp" "CMakeFiles/project_OOP.dir/project_OOP_autogen/3YJK5W5UP7/qrc_resources.cpp.obj" "gcc" "CMakeFiles/project_OOP.dir/project_OOP_autogen/3YJK5W5UP7/qrc_resources.cpp.obj.d"
+  "C:/Users/hqlapsg/Downloads/QuanlySinhVien/build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/project_OOP_autogen/mocs_compilation.cpp" "CMakeFiles/project_OOP.dir/project_OOP_autogen/mocs_compilation.cpp.obj" "gcc" "CMakeFiles/project_OOP.dir/project_OOP_autogen/mocs_compilation.cpp.obj.d"
+  "C:/Users/hqlapsg/Downloads/QuanlySinhVien/src/adminwindow.cpp" "CMakeFiles/project_OOP.dir/src/adminwindow.cpp.obj" "gcc" "CMakeFiles/project_OOP.dir/src/adminwindow.cpp.obj.d"
+  "C:/Users/hqlapsg/Downloads/QuanlySinhVien/src/databasemanager.cpp" "CMakeFiles/project_OOP.dir/src/databasemanager.cpp.obj" "gcc" "CMakeFiles/project_OOP.dir/src/databasemanager.cpp.obj.d"
+  "C:/Users/hqlapsg/Downloads/QuanlySinhVien/src/editstudentdialog.cpp" "CMakeFiles/project_OOP.dir/src/editstudentdialog.cpp.obj" "gcc" "CMakeFiles/project_OOP.dir/src/editstudentdialog.cpp.obj.d"
+  "C:/Users/hqlapsg/Downloads/QuanlySinhVien/src/gradedialog.cpp" "CMakeFiles/project_OOP.dir/src/gradedialog.cpp.obj" "gcc" "CMakeFiles/project_OOP.dir/src/gradedialog.cpp.obj.d"
+  "C:/Users/hqlapsg/Downloads/QuanlySinhVien/src/loginwindow.cpp" "CMakeFiles/project_OOP.dir/src/loginwindow.cpp.obj" "gcc" "CMakeFiles/project_OOP.dir/src/loginwindow.cpp.obj.d"
+  "C:/Users/hqlapsg/Downloads/QuanlySinhVien/src/main.cpp" "CMakeFiles/project_OOP.dir/src/main.cpp.obj" "gcc" "CMakeFiles/project_OOP.dir/src/main.cpp.obj.d"
+  "C:/Users/hqlapsg/Downloads/QuanlySinhVien/src/student.cpp" "CMakeFiles/project_OOP.dir/src/student.cpp.obj" "gcc" "CMakeFiles/project_OOP.dir/src/student.cpp.obj.d"
+  "C:/Users/hqlapsg/Downloads/QuanlySinhVien/src/studentwindow.cpp" "CMakeFiles/project_OOP.dir/src/studentwindow.cpp.obj" "gcc" "CMakeFiles/project_OOP.dir/src/studentwindow.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
