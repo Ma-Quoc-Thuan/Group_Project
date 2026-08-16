49908111ab61f207a9a14397c07f515201a55dd3
Ma Quoc Thuan <2512751437@student.hcmus.edu.vn>
2026-08-16 20:44:48 +0700

    Delete build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug directory

--- build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/CMakeFiles/project_OOP.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for project_OOP.
-# This may be replaced when dependencies are built.
--- build/Desktop_Qt_6_11_1_MinGW_64_bit_Debug/CMakeFiles/project_OOP.dir/project_OOP_autogen/3YJK5W5UP7/qrc_resources.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/project_OOP.dir/project_OOP_autogen/3YJK5W5UP7/qrc_resources.cpp.obj: \
- C:\Users\hqlapsg\Downloads\QuanlySinhVien\build\Desktop_Qt_6_11_1_MinGW_64_bit_Debug\project_OOP_autogen\3YJK5W5UP7\qrc_resources.cpp
